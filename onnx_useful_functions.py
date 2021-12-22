0a0ced2f8b2b5fccb5afee797e6a2d2f4643d6a6
Elvin <54735483+Elvin-Ma@users.noreply.github.com>
2021-12-22 20:10:54 +0800

    Update onnx_useful_functions.py

--- onnx_useful_functions.py
+++ onnx_useful_functions.py
@@ -10,6 +10,12 @@ def build_weight_data():
   bias_data = 2*np.ones([3],dtype=np.float32)
   weight_tensor=onnx.helper.make_tensor("conv1.weight",onnx.TensorProto.FLOAT,[3,1,3,3],weight_data.tobytes(),raw=True)
   bias_tensor=onnx.helper.make_tensor("conv1.bias",onnx.TensorProto.FLOAT,[3],bias_data.tobytes(),raw=True)
+
+def shape_infer():
+  model = onnx.load("resnet50_int8.onnx")
+  model = onnx.shape_inference.infer_shapes(model)
+  onnx.save(model, "resnet50_int8_shaped_temp.onnx")
+  netron.start("resnet50_int8_shaped_temp.onnx")
   
 if __name__ == "__main__":
   append_tensor_as_output()
