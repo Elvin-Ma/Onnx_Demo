d2fab38f3477a088815ef0cd039dee8df1ec96ad
Elvin <54735483+Elvin-Ma@users.noreply.github.com>
2022-02-07 12:54:45 +0800

    Create equal_conv_gen.py

---
+++ equal_conv_gen.py
@@ -0,0 +1,116 @@
+import onnx
+import netron
+import numpy as np
+
+def weight_gen(weight_name, weight_shape):
+  weight_data = np.zeros(weight_shape).astype(np.float32)
+  channel = weight_shape[1]
+  kernel_index = 0
+  for i in range(channel):
+    weight_data[i][kernel_index][0][0] = 1
+    kernel_index +=1
+
+  assert(weight_data.sum() == channel)
+  weight_tensor=onnx.helper.make_tensor(weight_name, onnx.TensorProto.FLOAT, weight_shape, weight_data.tobytes(), raw=True)
+  return weight_tensor
+
+def conv_gen(input_name, weight_name, output_name):
+  conv_node = onnx.helper.make_node(
+              'Conv',
+              inputs=[input_name, weight_name],
+              outputs=[output_name],
+              kernel_shape=[1, 1],
+              pads = [0,0,0,0]
+            )
+
+  return conv_node
+
+def onnx_insert_conv(model, input_tensor_name, weight_shape):
+  output_tensor_name = "equal_" + input_tensor_name
+  weight_name = "weight_" + input_tensor_name
+  weight = weight_gen(weight_name, weight_shape)
+  model.graph.initializer.append(weight)
+
+  conv = conv_gen(input_tensor_name, weight_name, output_tensor_name)
+
+  label = False
+
+  for node in model.graph.node:
+    for input in node.input:
+      if input == input_tensor_name and node.op_type != "Conv":
+        node.input.remove(input)
+        node.input.append(output_tensor_name)
+        label = True
+
+  assert(label)
+
+  model.graph.node.append(conv)
+  return model
+
+
+def set_input_shape(model, input_name, input_shape):
+  for input in model.graph.input:
+    model.graph.input.remove(input)
+
+  input_layer_value_info= onnx.helper.make_tensor_value_info(input_name, 1, input_shape)
+  model.graph.input.append(input_layer_value_info)
+
+  return model
+
+def shape_inference(src_path, dst_path):
+  model = onnx.load(src_path)
+  model = set_input_shape(model, "input_tensor:0", [1, 3, 224, 224])
+  model = onnx.shape_inference.infer_shapes(model)
+  onnx.save(model, dst_path)
+  # netron.start(dst_path)
+
+
+if __name__ == "__main__":
+  path = "/home/mtn/suinfer_temp/resnet50.onnx"
+  new_path = "resnet50_insert_conv.onnx"
+  resnet50_mlpert_onnx = "resnet50_v1.onnx"
+  resnet50_mlperf_onnx_equal_conv = "resnet50_mlperf_equal_conv.onnx"
+
+  shape_inference(resnet50_mlpert_onnx, resnet50_mlpert_onnx)
+  model = onnx.load(resnet50_mlpert_onnx)
+
+  # use for resnet50.onnx
+  # conv_info = [("336", [256, 256, 1, 1]),
+  #              ("346", [256, 256, 1, 1]),
+  #              ("368", [512, 512, 1, 1]),
+  #              ("378", [512, 512, 1, 1]),
+  #              ("388", [512, 512, 1, 1]),
+  #              ("410", [1024, 1024, 1, 1]),
+  #              ("420", [1024, 1024, 1, 1]),
+  #              ("430", [1024, 1024, 1, 1]),
+  #              ("440", [1024, 1024, 1, 1]),
+  #              ("450", [1024, 1024, 1, 1]),
+  #              ("472", [2048, 2048, 1, 1]),
+  #              ("482", [2048, 2048, 1, 1]),
+  #              ]
+
+  conv_info = [("resnet_model/Relu_3:0", [256, 256, 1, 1]),
+               ("resnet_model/Relu_6:0", [256, 256, 1, 1]),
+               ("resnet_model/Relu_12:0", [512, 512, 1, 1]),
+               ("resnet_model/Relu_15:0", [512, 512, 1, 1]),
+               ("resnet_model/Relu_18:0", [512, 512, 1, 1]),
+               ("resnet_model/Relu_24:0", [1024, 1024, 1, 1]),
+               ("resnet_model/Relu_27:0", [1024, 1024, 1, 1]),
+               ("resnet_model/Relu_30:0", [1024, 1024, 1, 1]),
+               ("resnet_model/Relu_33:0", [1024, 1024, 1, 1]),
+               ("resnet_model/Relu_36:0", [1024, 1024, 1, 1]),
+               ("resnet_model/Relu_42:0", [2048, 2048, 1, 1]),
+               ("resnet_model/Relu_45:0", [2048, 2048, 1, 1]),
+               ]
+
+
+  for item in conv_info:
+    model = onnx_insert_conv(model, item[0], item[1])
+
+  model = onnx.shape_inference.infer_shapes(model)
+  onnx.save(model, resnet50_mlperf_onnx_equal_conv)
+  netron.start(resnet50_mlperf_onnx_equal_conv)
+
+
+
+
